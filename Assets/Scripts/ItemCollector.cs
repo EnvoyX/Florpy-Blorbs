5d75e98925803dc1cedafd8cbb993dfedd8387ee
EnvoyX <muhamadhanifhafizhan@gmail.com>
2024-01-16 16:26:52 +0700

    added coins UI
    (further adjustment of coin position while in game will be updated it later)

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private Text coinText;
     private int coin = 0;
+    
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Coin")){
             ScoreManager.instance.coinScore(10000);
             Destroy(collision.gameObject);
             Debug.Log("You got 10000 points!");
             coin++;
+            coinText.text = "Coin: " + coin.ToString();
         }
     }
 }
